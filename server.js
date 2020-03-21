2c26e257beeff801716f3dd595910c4636136406
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-21 09:37:24 -0500

    Adding server.js stuff and routes

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+require('./routes/html-routes')(app);
+
 app.listen(PORT, () => {
   console.log(
     '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
