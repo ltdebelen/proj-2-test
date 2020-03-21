2a246f198638820e976d2a7e163ade621bd1dd29
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-03-21 08:26:57 -0500

    Updating eslint rule to stop tracking models/index.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(
+    '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+    PORT,
+    PORT
+  );
+});
